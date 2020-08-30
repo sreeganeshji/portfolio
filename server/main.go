f36f410c58ae50312ec17050f959415398873f3f
Ganesh <sree.ganeshji@gmail.com>
2020-08-29 19:42:18 -0700

    Initial commit

---
+++ server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+"io/ioutil"
+)
+
+type Page struct{
+	Title string
+	Body []byte
+}
+
+//save a page
+func (p *Page) save() error {
+	filename := p.Title+"txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func main(){
+
+	fmt.Print("Hello")
+}
